bc121dd73edbe67570692a5fef396a4dcd7b4a7d
Munshi Muzahid Hasan Tushar <36810834+muzahid-c@users.noreply.github.com>
2019-08-26 16:41:44 +0600

    added source code with details

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Named-Folder-Create
- Creating a folder using windows 'mkdir' command
+## Named Folder Create
+A python script to create a folder using `mkdir` command
+
+## Motivation
+When windows security policy is applied to prevent user to delete any file, only `mkdir` command can be used to create a folder with desired name. This program intension is to use that loophole/feature of windows to create a folder with name. 
+
+## Tech/framework used
+<b>Built with</b>
+- [Python](https://www.python.org) Python 3
+- [PyInstaller](https://www.pyinstaller.org) PyInstall is used to create the single exe. The commnd is `pyinstaller --onefile --windowed named-folder-create.py` 
+
+## License
+GNU General Public License v3.0
---
+++ named-folder-create.py
@@ -0,0 +1,30 @@
+# Create a named folder using windows/linux 'mkdir' command
+# Python 3 program
+# Author: Munshi Muzahid Hasan Tushar
+# Date: 26 August, 2019
+# Version: alpha 1.0 (Tkinter GUI)
+
+import os
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import messagebox
+from tkinter.messagebox import *
+
+
+
+root = tkinter.Tk()
+root.withdraw() # To hide Tkinter window
+root.iconbitmap(default=r'E:\Code\Named-Folder-Create\favicon.ico') # Tkinter custom icon, default will change icon in simpledialog also
+CurrDir = os.getcwd() # To get the base dir
+
+SelectDir = filedialog.askdirectory(parent=root, initialdir=CurrDir, title='Please select a directory') # Prompt to select the directory
+messagebox.showinfo("Selected Directory",SelectDir) # Showing selected direcory in information messagebox
+os.chdir(SelectDir) # Changing to selected directory
+
+
+FolderName = simpledialog.askstring(title='Create new folder', prompt='Enter folder name:') # New folder creation prompt
+CreatedFolder = os.mkdir(FolderName)
+messagebox.showinfo("Folder Created!",FolderName) # Success infomation messagebox
